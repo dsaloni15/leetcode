b1237c291feb9dfe049238d77e97ddd05f316f52
Saloni <164176187+dsaloni15@users.noreply.github.com>
2024-07-09 12:22:17 +0530

    Time: 1 ms (61.32%) | Memory: 41.7 MB (51.00%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i = 0 ; i < jewels.length() ;i++){
             for(int j = 0 ; j <stones.length() ;j++){
                 if(jewels.charAt(i) == stones.charAt(j)){
-                    count++;
+                    count += stones.charAt(j) == jewels.charAt(i) ? 1 : 0 ;
                 }
             }
         }
