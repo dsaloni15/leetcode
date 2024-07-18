843f57c81cdff7c37a36e3e852e48ebb82c72f54
Saloni <164176187+dsaloni15@users.noreply.github.com>
2024-07-18 20:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (9.50%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int b = 0;
+        int count = 0;
+
+        // for(char ch : s.toCharArray()){
+        //     if((b += ch == 'L'?1:-1)==0){
+        //         count++;
+        //     }
+        // }
+        // return count;
+
+        for(int i = 0;i<s.length() ; i++){
+            if(s.charAt(i) == 'R'){
+                b++;
+            }
+            else{
+                b--;
+            }
+            if(b == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
