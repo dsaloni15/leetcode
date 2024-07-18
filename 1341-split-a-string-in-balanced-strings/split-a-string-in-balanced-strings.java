36d5a8283ec3e172cee07a0816590dc9fb3b9526
Saloni <164176187+dsaloni15@users.noreply.github.com>
2024-07-18 20:16:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (60.56%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -3,24 +3,24 @@ class Solution {
         int b = 0;
         int count = 0;
 
-        // for(char ch : s.toCharArray()){
-        //     if((b += ch == 'L'?1:-1)==0){
-        //         count++;
-        //     }
-        // }
-        // return count;
-
-        for(int i = 0;i<s.length() ; i++){
-            if(s.charAt(i) == 'R'){
-                b++;
-            }
-            else{
-                b--;
-            }
-            if(b == 0){
+        for(char ch : s.toCharArray()){
+            if((b += ch == 'L'?1:-1)==0){
                 count++;
             }
         }
         return count;
+
+        // for(int i = 0;i<s.length() ; i++){
+        //     if(s.charAt(i) == 'R'){
+        //         b++;
+        //     }
+        //     else{
+        //         b--;
+        //     }
+        //     if(b == 0){
+        //         count++;
+        //     }
+        // }
+        // return count;
     }
 }
