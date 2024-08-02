efe34b59d14ccb2d01a7f7f4fdb593815dfae951
Saloni <164176187+dsaloni15@users.noreply.github.com>
2024-08-02 16:38:21 +0530

    Time: 3 ms (98.50%) | Memory: 44.8 MB (33.05%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.java
+++ 2886-faulty-keyboard/faulty-keyboard.java
@@ -1,13 +1,18 @@
 class Solution {
     public String finalString(String s) {
-        StringBuilder sb = new StringBuilder();
+         StringBuilder sb = new StringBuilder();
+        // for(int i = 0 ; i < s.length() ; i++){
+        //     char c = s.charAt(i);
+        //     if(c != 'i'){
+        //         sb.append(c);
+        //     }else if(c == 'i'){
+        //         sb.reverse();
+        //     }
+        // }
+        // return sb.toString();
         for(int i = 0 ; i < s.length() ; i++){
-            char c = s.charAt(i);
-            if(c != 'i'){
-                sb.append(c);
-            }else if(c == 'i'){
-                sb.reverse();
-            }
+            if(s.charAt(i) != 'i') sb.append(s.charAt(i));
+            else if(s.charAt(i) == 'i') sb.reverse();
         }
         return sb.toString();
     }
