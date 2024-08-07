26180d3502d9c28d107e9c77e4eb9978d1d894c6
Saloni <164176187+dsaloni15@users.noreply.github.com>
2024-08-07 22:39:36 +0530

    Time: 1 ms (62.04%) | Memory: 44.1 MB (43.10%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
@@ -1,24 +1,42 @@
 class Solution {
     public boolean canAliceWin(int[] nums) {
-        int[] doubleDig = new int [nums.length];
-        int[] singleDig = new int [nums.length];
-        int singleSum = 0;
-        int doubleSum = 0;
+        // int[] doubleDig = new int [nums.length];
+        // int[] singleDig = new int [nums.length];
+        // int singleSum = 0;
+        // int doubleSum = 0;
 
-        for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i] > 9){
-                doubleDig[i] = nums[i];
-            }else{
-                singleDig[i] = nums[i];
+        // for(int i = 0 ; i < nums.length ; i++){
+        //     if(nums[i] > 9){
+        //         doubleDig[i] = nums[i];
+        //     }else{
+        //         singleDig[i] = nums[i];
+        //     }
+        // }
+        // for(int i = 0 ; i < nums.length ; i++){
+        //     singleSum += singleDig[i];
+        // }
+        // for(int i = 0 ; i < nums.length ; i++){
+        //     doubleSum += doubleDig[i];
+        // }
+        // if(singleSum == doubleSum){
+        //     return false;
+        // }
+        // return true;
+        int sum1=0;
+        int sum2=0;
+        for(int i:nums)
+        {
+            if(i>9)
+            {
+                sum1=sum1+i;
             }
-        }
-        for(int i = 0 ; i < nums.length ; i++){
-            singleSum += singleDig[i];
-        }
-        for(int i = 0 ; i < nums.length ; i++){
-            doubleSum += doubleDig[i];
-        }
-        if(singleSum == doubleSum){
+            else
+            {
+                sum2=sum2+i;
+            }
+         }
+        if(sum1==sum2)
+        {
             return false;
         }
         return true;
