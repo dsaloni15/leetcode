3632ea01dacdb65ab61e7f5ef79f4eac67262428
Saloni <164176187+dsaloni15@users.noreply.github.com>
2024-10-01 23:12:11 +0530

    Time: 2 ms (95.10%) | Memory: 46.4 MB (69.81%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,21 +1,10 @@
 class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n+1];
-
-        ans[0] = 0;
+        
         for(int i = 0 ; i <= n ; i++){
-            ans[i] = bin(i);
+            ans[i] = Integer.bitCount(i);
         }
         return ans;
-    }
-    public int bin(int i){
-        int tot = 0;
-        while(i > 0){
-            if(i%2 == 1){
-                tot++;
-            }
-            i /= 2;
-        }
-        return tot;
     }
 }
