9cf04e746c5db9486702a1d34cebe3e1884a6096
Saloni <164176187+dsaloni15@users.noreply.github.com>
2025-12-07 15:38:15 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (6.29%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -2,12 +2,12 @@ class Solution {
     public int maximumWealth(int[][] accounts) {
         int ans = Integer.MIN_VALUE;
         for(int i = 0 ; i < accounts.length ; i++){
-            int sum = 0;
+            int curr = 0;
             for(int j = 0 ; j < accounts[i].length ; j++){
-                sum += accounts[i][j];
+                curr += accounts[i][j];
             }
-            if(sum > ans){
-                ans = sum;
+            if(ans < curr){
+                ans = curr;
             }
         }
         return ans;
