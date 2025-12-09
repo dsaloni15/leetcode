47f5a112d9a8053650657ed7fd60286b90ff4c35
Saloni <164176187+dsaloni15@users.noreply.github.com>
2025-12-09 14:41:57 +0530

    Time: 1 ms (93.11%) | Memory: 42.4 MB (43.36%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,25 +1,24 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x == 0 || x==1){
+        if(x==0 || x==1){
             return x;
         }
         int i = 1;
         int j = x;
-        int mid = -1;
 
         while(i <= j){
-            mid = i + (j - i)/2;
+            int mid = i + (j-i)/2;
 
             if(Math.pow(mid,2) == x){
                 return mid;
             }
             if(Math.pow(mid,2) > x){
-                j = mid - 1;
+                j = mid-1;
             }
             else{
-                i = mid + 1;
+                i = mid+1;
             }
         }
-        return Math.round(j);
+        return j;
     }
 }
